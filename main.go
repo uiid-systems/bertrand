4c4e049d32a8b5864b905d6dca7fb71e6ae374ff
Adam Fratino <fratino.a@gmail.com>
2026-03-09 13:47:39 -0400

    Add GoReleaser config, release workflow, and version flag
    Sets up Homebrew tap distribution via uiid-systems/homebrew-tap.

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"github.com/uiid-systems/bertrand/cmd"
 )
 
+var version = "dev"
+
 func main() {
+	cmd.SetVersion(version)
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Fprintf(os.Stderr, "bertrand panic: %v\n", r)
