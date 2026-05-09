e2232a6a7a69fdd7815358b75c301b718033b51a
Adam Fratino <hello@fratino.dev>
2026-05-09 15:23:09 -0400

    tinkering

--- dashboard/src/routes/__root.tsx
+++ dashboard/src/routes/__root.tsx
@@ -53,7 +53,7 @@ function AppShell() {
     <Stack fullwidth style={{ position: "fixed", height: "100dvh" }}>
       <TopBar sessionCount={sessions.length} />
       <Resizable direction="horizontal">
-        <ResizablePanel defaultSize={380} minSize={320} maxSize={540}>
+        <ResizablePanel defaultSize={360} minSize={320} maxSize={540}>
           <Sidebar sessions={sessions} />
         </ResizablePanel>
 
@@ -64,7 +64,7 @@ function AppShell() {
           </Stack>
         </ResizablePanel>
         <ResizableHandle />
-        <ResizablePanel defaultSize={460} minSize={380} maxSize={540}>
+        <ResizablePanel defaultSize={360} minSize={380} maxSize={540}>
           <SecondarySidebar />
         </ResizablePanel>
       </Resizable>
